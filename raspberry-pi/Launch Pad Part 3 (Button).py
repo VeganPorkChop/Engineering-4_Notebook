06a4f170016aa74d3b339d720f2a3f4c58941345
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2023-09-08 10:13:08 -0400

    finished launch pad 3

---
+++ raspberry-pi/Launch Pad Part 3 (Button).py
@@ -0,0 +1,30 @@
+import board
+import time
+import digitalio
+
+ledRed = digitalio.DigitalInOut(board.GP0)
+ledRed.direction = digitalio.Direction.OUTPUT
+ledGreen = digitalio.DigitalInOut(board.GP1)
+ledGreen.direction = digitalio.Direction.OUTPUT
+button_a = digitalio.DigitalInOut(board.GP15)
+button_a.direction = digitalio.Direction.INPUT
+button_a.pull = digitalio.Pull.UP
+count = 0
+
+while True:
+    if button_a.value == False:
+        for x in range(10, -1 ,-1):
+            if x > 0:
+                print(x)
+            if x == 0:
+                print('LAUNCH')
+                ledGreen.value = True
+                print('Countdown Finished!')
+                time.sleep(5)
+            ledRed.value = True
+            time.sleep(0.5)
+            ledRed.value = False
+            time.sleep(0.5)
+    else:
+        ledGreen.value = False
+        print('Waiting For Button')
