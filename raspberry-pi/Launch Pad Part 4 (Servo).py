3107352fb0fc368c7d15860bf988b1359f55ccb8
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2023-09-14 10:48:16 -0400

    idk man, its a hard day

--- raspberry-pi/Launch Pad Part 4 (Servo).py
+++ raspberry-pi/Launch Pad Part 4 (Servo).py
@@ -2,7 +2,9 @@ import board
 import time
 import digitalio
 import pwmio
+import simpleio
 from adafruit_motor import servo
+
 
 pwm_servo = pwmio.PWMOut(board.GP22, duty_cycle=2 ** 15, frequency=50)
 servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
@@ -14,37 +16,36 @@ button_a = digitalio.DigitalInOut(board.GP15)
 button_a.direction = digitalio.Direction.INPUT
 button_a.pull = digitalio.Pull.UP
 st = time.monotonic()
-lt = 100000000000000000000
 z = 0
-
+cd = 0
+bp = 0
+ABORTER = 1
+t = False
 while True:
-    if button_a.value == False:
-        for x in range(10, -1 ,-1):
-            ct = time.monotonic()
-            tp = ct-st
-            if tp >= 1/60:
-                z = 1
-            if x <= 3 and z == 1:
-                for y in range(0, 181, 1):
-                    servo1.angle = y
-            if x > 0:
-                print(x)
-            elif x == 0:
-                print('LAUNCH')
-                ledGreen.value = True
-                print('Countdown Finished!')
-            ledRed.value = True
-            ct = time.monotonic()
-            tt = ct + .5
-            while ct < tt:
-                ct = time.monotonic()
-                print(ct)
-            ct = time.monotonic()
-            ledRed.value = False
-            tt = ct + .5
-            while ct < tt:
-                ct = time.monotonic()
-                print(ct)
+    if button_a.value == False and ABORTER == 1 and t == True:
+        ABORTER = 0
+        ct = time.monotonic()
+        x = st - ct
+        cd = 10 - x
+        if cd <= 3:
+            for y in range(0, 181, 1):
+                servo1.angle = y
+        if cd > 0:
+            print(cd)
+        if cd == 0:
+            print('LAUNCH')
+            ledGreen.value = True
+            print('Countdown Finished!')
+        if 0.5 >= st - (ct + 0.5*bp):
+            bp = bp+ 1
+            led = True
+            if led == True:
+                ledRed = False
+                led = False
+            ledRed = True
+    elif button_a.value == False and ABORTER == 0:
+        print("ABORT")
+        ABORTER = 1
     else:
         z = 0
         servo1.angle = 0
