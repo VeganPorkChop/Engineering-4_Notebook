5d3cf202b5d296f86d38ee2be17986cd4a291f94
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2023-09-15 10:21:32 -0400

    Launch pad wrok

--- raspberry-pi/Launch Pad Part 4 (Servo).py
+++ raspberry-pi/Launch Pad Part 4 (Servo).py
@@ -1,53 +1,46 @@
-import board
+import board                                   
 import time
 import digitalio
 import pwmio
-import simpleio
-from adafruit_motor import servo
+from adafruit_motor import servo # import from 8x.x package on adafruit website
 
+pwm_servo = pwmio.PWMOut(board.GP22, duty_cycle=2 ** 15, frequency=50) # servo setup PWM PIN
+redLED = digitalio.DigitalInOut(board.GP0) 
+greenLED = digitalio.DigitalInOut(board.GP1)
+button = digitalio.DigitalInOut(board.GP15)
+redLED.direction = digitalio.Direction.OUTPUT
+greenLED.direction = digitalio.Direction.OUTPUT
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500) # Servo PULSE SIZE
 
-pwm_servo = pwmio.PWMOut(board.GP22, duty_cycle=2 ** 15, frequency=50)
-servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
-ledRed = digitalio.DigitalInOut(board.GP0)
-ledRed.direction = digitalio.Direction.OUTPUT
-ledGreen = digitalio.DigitalInOut(board.GP1)
-ledGreen.direction = digitalio.Direction.OUTPUT
-button_a = digitalio.DigitalInOut(board.GP15)
-button_a.direction = digitalio.Direction.INPUT
-button_a.pull = digitalio.Pull.UP
-st = time.monotonic()
-z = 0
-cd = 0
-bp = 0
-ABORTER = 1
-t = False
+servo1.angle = 0 # angle of servo
+count = 0                        
+
 while True:
-    if button_a.value == False and ABORTER == 1 and t == True:
-        ABORTER = 0
-        ct = time.monotonic()
-        x = st - ct
-        cd = 10 - x
-        if cd <= 3:
-            for y in range(0, 181, 1):
-                servo1.angle = y
-        if cd > 0:
-            print(cd)
-        if cd == 0:
-            print('LAUNCH')
-            ledGreen.value = True
-            print('Countdown Finished!')
-        if 0.5 >= st - (ct + 0.5*bp):
-            bp = bp+ 1
-            led = True
-            if led == True:
-                ledRed = False
-                led = False
-            ledRed = True
-    elif button_a.value == False and ABORTER == 0:
-        print("ABORT")
-        ABORTER = 1
-    else:
-        z = 0
-        servo1.angle = 0
-        ledGreen.value = False
-        print('Waiting For Button')
+    print('waiting for button')
+    if button.value == False:
+        count = 10
+    while count > 3:                               
+        print(count)
+        redLED.value = True   
+        count = count - 1
+        time.sleep(.5)
+        redLED.value = False
+        time.sleep(.5)
+    while count > 0 & count <= 3:
+        print(count)
+        redLED.value = True
+        count = count - 1
+        for x in range(10):
+            servo1.angle = servo1.angle + 3
+            time.sleep(.05)
+        redLED.value = False
+        for x in range(10):
+            servo1.angle = servo1.angle + 3
+            time.sleep(.05)
+        if count == 0:                              
+            print("launch")
+            greenLED.value = True
+            servo1.angle = 180
+            time.sleep(5)
